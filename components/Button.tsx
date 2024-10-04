326d9225af9862bbc5093b5bf9d709fb40df4ad2
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-10-04 09:44:18 +0800

    Add transition to the bg-color for buttons

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Button({ children }: { children: React.ReactNode }) {
   return (
-    <button className="rounded-md bg-[#22317c] px-6 py-3 text-sm font-semibold text-[#edf2ff] hover:bg-[#384689]">
+    <button className="rounded-md bg-[#22317c] px-6 py-3 text-sm font-semibold text-[#edf2ff] transition-colors hover:bg-[#384689]">
       {children}
     </button>
   );
