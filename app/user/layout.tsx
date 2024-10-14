96cfbd037c23d1b1a466a9de5041933297b00922
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-10-14 08:46:46 +0800

    Add a layout for the user route

---
+++ app/user/layout.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Provider from "@/app/Provider";
+import { auth } from "@/lib/auth";
+import { hasUser } from "@/lib/utils";
+
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+
+export default async function layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+
+  return (
+    <div className="flex">
+      <Provider>
+        <Sidebar session={hasUser(session) ? session : null} />
+        <main className="h-full w-full overflow-auto px-5 py-5 md:px-8">
+          <Header />
+          {children}
+        </main>
+      </Provider>
+    </div>
+  );
+}
