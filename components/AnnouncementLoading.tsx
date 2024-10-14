00fa6baf41b847f77c8f614ec83d4f3ceb8c893f
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-10-14 08:44:33 +0800

    Add a loading component for posts/announcements
    - Add missing loading components to other routes

---
+++ components/AnnouncementLoading.tsx
@@ -0,0 +1,32 @@
+export default function AnnouncementLoading() {
+  return (
+    <ul className="grid gap-4">
+      <li className="flex w-full flex-col gap-4 rounded-md border-2 border-[#dbe4ff] bg-[#f3f6ff] p-4">
+        <div className="flex gap-2">
+          <div className="h-12 w-12 rounded-full bg-[#dbe4ff] md:h-16 md:w-16"></div>
+          <div className="flex flex-col gap-2">
+            <div className="h-3 w-[10rem] rounded-md bg-[#dbe4ff] md:h-5 md:w-[20rem]"></div>
+            <div className="h-2 w-[5rem] rounded-md bg-[#dbe4ff] md:h-3 md:w-[10rem]"></div>
+            <div className="h-2 w-[3rem] rounded-md bg-[#dbe4ff] md:h-3 md:w-[5rem]"></div>
+          </div>
+        </div>
+        <div className="h-3 w-[10rem] rounded-md bg-[#dbe4ff] md:h-5 md:w-[20rem]"></div>
+        <div className="h-2 w-[14rem] rounded-md bg-[#dbe4ff] md:h-4 md:w-[25rem]"></div>
+        <div className="h-[15rem] w-full rounded-md bg-[#dbe4ff]"></div>
+      </li>
+      <li className="flex w-full flex-col gap-4 rounded-md border-2 border-[#dbe4ff] bg-[#f3f6ff] p-4">
+        <div className="flex gap-2">
+          <div className="h-12 w-12 rounded-full bg-[#dbe4ff] md:h-16 md:w-16"></div>
+          <div className="flex flex-col gap-2">
+            <div className="h-3 w-[10rem] rounded-md bg-[#dbe4ff] md:h-5 md:w-[20rem]"></div>
+            <div className="h-2 w-[5rem] rounded-md bg-[#dbe4ff] md:h-3 md:w-[10rem]"></div>
+            <div className="h-2 w-[3rem] rounded-md bg-[#dbe4ff] md:h-3 md:w-[5rem]"></div>
+          </div>
+        </div>
+        <div className="h-3 w-[10rem] rounded-md bg-[#dbe4ff] md:h-5 md:w-[20rem]"></div>
+        <div className="h-2 w-[14rem] rounded-md bg-[#dbe4ff] md:h-4 md:w-[25rem]"></div>
+        <div className="h-[15rem] w-full rounded-md bg-[#dbe4ff]"></div>
+      </li>
+    </ul>
+  );
+}
