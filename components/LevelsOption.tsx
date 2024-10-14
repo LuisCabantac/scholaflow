1fdbefe79093c7f888e742308e21fac01a664137
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-10-14 08:52:53 +0800

    Add a level option for the announcement form

---
+++ components/LevelsOption.tsx
@@ -0,0 +1,32 @@
+import { ILevels } from "@/app/user/announcements/page";
+
+function capitalizeFirstLetter(str: string) {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+export default function LevelsOption({
+  allLevels,
+  defaultValue,
+}: {
+  allLevels?: ILevels[];
+  defaultValue?: string;
+}) {
+  return (
+    <select
+      required
+      name="levels"
+      className="level__select cursor-pointer rounded-md bg-[#ced8f7] px-4 py-2 text-sm md:px-5 md:py-3 md:text-base"
+      defaultValue={defaultValue ? defaultValue : "all levels"}
+    >
+      <option value="all levels">All Levels</option>
+      {/* {allLevels.map((level) => (
+        <option key={level.id} value={level.level}>
+          {capitalizeFirstLetter(level.level)}
+        </option>
+      ))} */}
+    </select>
+  );
+}
