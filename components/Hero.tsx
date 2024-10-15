77db4b72745e1e3c30b9ec22324272faf6cf0a3e
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-10-15 13:41:00 +0800

    Update hero section's description

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,24 +6,20 @@ import Button from "@/components/Button";
 
 export default function Hero() {
   return (
-    <section className="items-center gap-4 px-6 py-16 md:grid md:grid-cols-[4fr_3fr] md:px-12 md:py-32">
-      <div className="flex flex-col items-start gap-y-4">
+    <section className="items-center gap-4 px-6 py-24 md:grid md:grid-cols-[4fr_3fr] md:px-12 md:py-24">
+      <div className="flex flex-col items-start gap-4">
         <h1 className="text-3xl font-bold md:text-5xl">
           Your All-in-One Solution for a Seamless Learning Experience
         </h1>
         <p>
-          Experience the future of education with ScholaFlow. Our platform
-          integrates virtual classrooms, secure data storage, and detailed
-          performance analytics into one easy-to-use tool. Say goodbye to
-          complicated management and hello to a brighter educational journey for
-          both students and educators. Let&apos;s make learning effortless and
-          effective together.
+          Virtual classrooms and essential tools, all in one. Make learning
+          easier.
         </p>
         <Button href="/user/dashboard" type="primary">
           Get started
         </Button>
       </div>
-      <div className="relative w-full">
+      <div className="relative w-full pt-4 md:pt-0">
         <Image
           src={landingPageImage1}
           alt="road to knowledge"
