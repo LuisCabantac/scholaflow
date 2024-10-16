af519b3b649215637edd6dd5d11d3ad030cdcba8
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-10-17 04:31:04 +0800

    Show an access denied text and image when the user has a role of guest
    - Adjust no announcements' gap between the image and text

--- app/user/dashboard/page.tsx
+++ app/user/dashboard/page.tsx
@@ -1,12 +1,16 @@
 import { redirect } from "next/navigation";
 
 import { auth } from "@/lib/auth";
+import { hasUser } from "@/lib/utils";
 
 import DashboardSection from "@/components/DashboardSection";
+import AccessDenied from "@/components/AccessDenied";
 
 export default async function Page() {
   const session = await auth();
-  if (!session) return redirect("/signin");
+  if (!hasUser(session)) return redirect("/signin");
+
+  if (session.user.role === "guest") return <AccessDenied />;
 
   return <DashboardSection />;
 }
--- components/AccessDenied.tsx
+++ components/AccessDenied.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
-import noPosts from "@/public/app/no_posts.svg";
+import noPosts from "@/public/app/access_denied.svg";
 
-export default function NoAnnouncement() {
+export default function AccessDenied() {
   return (
     <div className="flex h-[30rem] w-full flex-col items-center justify-center gap-3 md:h-[25rem] md:gap-2">
       <div className="relative w-[15rem] md:w-[20rem]">
         <Image src={noPosts} alt="no posts" className="object-cover" />
       </div>
       <p className="font-medium md:text-lg">
-        No announcements have been made yet.
+        Please log in with a student or teacher account to access this page.
       </p>
     </div>
   );
