868fb3cebfb9c4af0ecdf4f215d5eb258fa79c6f
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-10-16 09:15:24 +0800

    Revert "Temporarily remove nunito sans font"
    This reverts commit 254d3b68f929ac479dbb9f757434541b8212af1e.

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
-// import { Nunito_Sans } from "next/font/google";
+import { Nunito_Sans } from "next/font/google";
 
-// import { FontType } from "@/app/layout";
+import { FontType } from "@/app/layout";
 import scholaflowLogo from "@/public/scholaflow_logo.png";
 
-// const nunitoSans: FontType = Nunito_Sans({
-//   subsets: ["latin"],
-//   display: "swap",
-// });
+const nunitoSans: FontType = Nunito_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Logo() {
   return (
@@ -15,10 +15,9 @@ export default function Logo() {
       <div className="relative h-7 w-7 md:h-8 md:w-8">
         <Image src={scholaflowLogo} alt="logo" fill className="object-cover" />
       </div>
-      <p className="cursor-default text-xl font-bold text-[#22317c] md:text-2xl">
-        {/* <p
+      <p
         className={`${nunitoSans.className} cursor-default text-xl font-bold text-[#22317c] md:text-2xl`}
-      > */}
+      >
         ScholaFlow
       </p>
     </div>
