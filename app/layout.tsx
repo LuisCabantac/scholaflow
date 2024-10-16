48922ce1508edbb65bffff12762a75d7e3938feb
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-10-16 10:58:30 +0800

    Revert "Change the body font to Plus Jakarta Sans"
    This reverts commit 17c2f2d41dddc15c93d5b85163de89bb9ee60d7a.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Plus_Jakarta_Sans } from "next/font/google";
+import { Poppins } from "next/font/google";
 
 import "@/styles/globals.css";
 
@@ -8,10 +8,10 @@ export const metadata: Metadata = {
   description: "Your All-in-One Solution for a Seamless Learning Experience",
 };
 
-const plusJakartaSans = Plus_Jakarta_Sans({
+const poppins = Poppins({
   subsets: ["latin"],
   display: "swap",
-  weight: ["300", "400", "500", "600", "700"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export interface FontType {
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${plusJakartaSans.className} box-border bg-[#edf2ff] text-[#33383d] antialiased`}
+        className={`${poppins.className} box-border bg-[#edf2ff] text-[#33383d] antialiased`}
       >
         {children}
       </body>
