44d91b349b48f87ed6120e085533bd9906b632bf
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-10-19 06:02:10 +0800

    Add user, class, and program management routes for the admin page

--- app/user/admin/page.tsx
+++ app/user/admin/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 import { auth } from "@/lib/auth";
 import { hasUser } from "@/lib/utils";
+
+import userManage from "@/public/app/user_management.svg";
+import programManage from "@/public/app/program_management.svg";
+import classManage from "@/public/app/class_management.svg";
 
 export default async function Page() {
   const session = await auth();
@@ -10,5 +16,44 @@ export default async function Page() {
 
   if (session.user.role !== "admin") return redirect("/user/dashboard");
 
-  return <div>HELLO ADMIN</div>;
+  return (
+    <section className="grid gap-4">
+      <div className="grid gap-2">
+        <h2 className="text-lg font-medium md:text-xl">User Management</h2>
+        <Link
+          href="/user/admin/user-management"
+          className="flex items-center gap-8 rounded-md border-2 bg-[#f3f6ff] p-4 md:text-lg"
+        >
+          <div className="relative w-[10rem]">
+            <Image src={userManage} alt="user management" />
+          </div>
+          <span>Manage roles and user accounts.</span>
+        </Link>
+      </div>
+      <div className="grid gap-2">
+        <h2 className="text-lg font-medium md:text-xl">Program Management</h2>
+        <Link
+          href="/user/admin/program-management"
+          className="flex items-center gap-8 rounded-md border-2 bg-[#f3f6ff] p-4 md:text-lg"
+        >
+          <div className="relative w-[10rem]">
+            <Image src={programManage} alt="program management" />
+          </div>
+          <span>Manage programs and courses for all levels.</span>
+        </Link>
+      </div>
+      <div className="grid gap-2">
+        <h2 className="text-lg font-medium md:text-xl">Class Management</h2>
+        <Link
+          href="/user/admin/class-management"
+          className="flex items-center gap-8 rounded-md border-2 bg-[#f3f6ff] p-4 md:text-lg"
+        >
+          <div className="relative w-[10rem]">
+            <Image src={classManage} alt="class management" />
+          </div>
+          <span>Organize class sections</span>
+        </Link>
+      </div>
+    </section>
+  );
 }
