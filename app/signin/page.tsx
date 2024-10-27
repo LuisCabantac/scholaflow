56137df499510752d19e29589defe4e9025da259
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-10-27 10:15:30 +0800

    Replace the sign-in image in the sign-in page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -10,7 +10,7 @@ import SignInForm from "@/components/SignInForm";
 import SignInGoogleButton from "@/components/SignInGoogleButton";
 
 export const metadata: Metadata = {
-  title: "Sign in",
+  title: "Sign-in",
 };
 
 export default async function Page() {
@@ -19,22 +19,24 @@ export default async function Page() {
   if (session) return redirect("/user/dashboard");
 
   return (
-    <section className="flex items-center justify-center px-8 py-[8rem] md:grid md:grid-cols-[60%_40%] md:px-24 md:py-24">
-      <div className="relative mx-0 my-auto w-0 md:w-[70%]">
-        <Image
-          src={signInPageImage}
-          alt="road to knowledge"
-          className="object-cover"
-        />
-      </div>
-      <div className="flex w-full flex-col justify-center gap-10">
-        <Logo />
-        <h2 className="text-3xl font-bold md:text-4xl">Welcome back!</h2>
-        <div className="grid gap-3">
-          <SignInForm />
-          <SignInGoogleButton />
+    <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(237,242,255,0.3),rgba(255,255,255,0))]">
+      <section className="flex items-center justify-center px-8 py-[8rem] md:grid md:grid-cols-[60%_40%] md:px-24 md:py-24">
+        <div className="relative mx-0 my-auto w-0 md:w-[70%]">
+          <Image
+            src={signInPageImage}
+            alt="road to knowledge"
+            className="object-cover"
+          />
         </div>
-      </div>
-    </section>
+        <div className="flex w-full flex-col justify-center gap-10">
+          <Logo />
+          <h2 className="text-3xl font-semibold md:text-5xl">Welcome back!</h2>
+          <div className="grid gap-3">
+            <SignInForm />
+            <SignInGoogleButton />
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
