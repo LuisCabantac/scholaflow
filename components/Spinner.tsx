b96641df1e5f9a7e700888b6329e654864f0470e
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-10-27 11:08:03 +0800

    Increase the height of the spinner for desktop screens

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,6 +1,6 @@
 export default function Spinner() {
   return (
-    <div className="flex h-[45rem] items-center justify-center md:h-[25rem]">
+    <div className="flex h-[45rem] items-center justify-center md:h-[35rem]">
       <div className="spinner"></div>
     </div>
   );
