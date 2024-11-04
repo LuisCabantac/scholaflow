303ad3b2a371c50dfffe5a3182ce24950bfefa97
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-11-04 09:45:47 +0800

    Add an image to the hero section of the landing page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,11 @@
 import Button from "@/components/Button";
+import heroImage from "@/public/landing_page/hero.png";
+import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="z-10">
-      <section className="flex items-center justify-center gap-4 px-4 py-44 text-center md:px-48 md:py-[9rem]">
+    <div className="z-10 flex flex-col items-center">
+      <section className="flex items-center justify-center gap-4 px-4 pb-10 pt-20 text-center md:px-48 md:pb-10 md:pt-14">
         <div className="flex flex-col items-center justify-center gap-4 md:gap-8">
           <div className="grid gap-6">
             <h1 className="text-3xl font-semibold md:text-6xl">
@@ -25,6 +27,14 @@ export default function Hero() {
           </div>
         </div>
       </section>
+      <Image
+        src={heroImage}
+        width={0}
+        height={0}
+        placeholder="blur"
+        className="h-[90%] w-[90%] rounded-md shadow-lg md:w-[70%]"
+        alt="hero image"
+      />
     </div>
   );
 }
