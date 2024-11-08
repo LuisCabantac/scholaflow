467356bb4a47c32902fc7c8b512a586907005429
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-11-09 03:32:05 +0800

    Remove border between the role indicator and other texts in the profile icon

--- components/ProfileIcon.tsx
+++ components/ProfileIcon.tsx
@@ -9,19 +9,15 @@ import { useClickOutside } from "@/contexts/ClickOutsideContext";
 import SignOutButton from "@/components/SignOutButton";
 
 export default function ProfileIcon({
-  avatar,
-  email,
-  fullName,
   role,
-  school,
-  course,
+  email,
+  avatar,
+  fullName,
 }: {
-  avatar: string;
+  role: string;
   email: string;
+  avatar: string;
   fullName: string;
-  role?: string;
-  school?: string | null;
-  course?: string;
 }) {
   const [isOpenPopover, setIsOpenPopover] = useState(false);
   const { useClickOutsideHandler } = useClickOutside();
@@ -67,28 +63,15 @@ export default function ProfileIcon({
               <li className="text-xs text-[#6e7280]">{email}</li>
             </div>
           </div>
-          <div className="grid w-full gap-2 border-t-2 border-[#d6e0fd] pt-2">
+          <div className="grid w-full gap-2">
             <li
               className={`role flex items-center justify-center rounded-md ${role === "admin" ? "admin" : role === "teacher" ? "teacher" : role === "student" ? "student" : "guest"} p-1 text-[0.65rem] font-semibold uppercase md:p-2 md:text-xs`}
             >
               {role}
             </li>
-            {school && (
-              <li className="flex gap-2">
-                <svg viewBox="0 0 24 24" className="size-5 fill-[#5c7cfa]">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
-                  />
-                </svg>
-                <span className="text-sm font-medium">{school}</span>
-              </li>
-            )}
-            {course && <li className="text-xs text-[#616572]">{course}</li>}
           </div>
         </div>
-        <li className="border-t-2 border-[#d6e0fd] pt-2">
+        <li>
           <Link
             href="/user/profile"
             className="flex gap-[0.6rem] rounded-md p-2 text-sm transition-colors hover:bg-[#c7d2f1]"
@@ -102,29 +85,6 @@ export default function ProfileIcon({
               />
             </svg>
             <span>Profile</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/user/dashboard"
-            className="flex gap-[0.6rem] rounded-md p-2 text-sm transition-colors hover:bg-[#c7d2f1]"
-            onClick={handleToggleOpenPopover}
-          >
-            <svg
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              className="size-5 fill-transparent stroke-[#5c7cfa]"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 3 H9 A1 1 0 0 1 10 4 V11 A1 1 0 0 1 9 12 H4 A1 1 0 0 1 3 11 V4 A1 1 0 0 1 4 3 z"
-              />
-              <path d="M15 3 H20 A1 1 0 0 1 21 4 V7 A1 1 0 0 1 20 8 H15 A1 1 0 0 1 14 7 V4 A1 1 0 0 1 15 3 z" />
-              <path d="M15 12 H20 A1 1 0 0 1 21 13 V20 A1 1 0 0 1 20 21 H15 A1 1 0 0 1 14 20 V13 A1 1 0 0 1 15 12 z" />
-              <path d="M4 16 H9 A1 1 0 0 1 10 17 V20 A1 1 0 0 1 9 21 H4 A1 1 0 0 1 3 20 V17 A1 1 0 0 1 4 16 z" />
-            </svg>
-            <span>Dashboard</span>
           </Link>
         </li>
         <li>
