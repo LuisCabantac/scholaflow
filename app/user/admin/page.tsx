870e140da5cb4c9371d5320165ff5c6e44e8b3ec
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-11-09 03:32:47 +0800

    Remove program management from the admin page

--- app/user/admin/page.tsx
+++ app/user/admin/page.tsx
@@ -6,7 +6,6 @@ import { auth } from "@/lib/auth";
 import { hasUser } from "@/lib/utils";
 
 import userManage from "@/public/app/user_management.svg";
-import programManage from "@/public/app/program_management.svg";
 
 export default async function Page() {
   const session = await auth();
@@ -27,18 +26,6 @@ export default async function Page() {
             <Image src={userManage} alt="user management" />
           </div>
           <span>Manage roles and user accounts.</span>
-        </Link>
-      </div>
-      <div className="grid gap-2">
-        <h2 className="text-lg font-medium md:text-xl">Program Management</h2>
-        <Link
-          href="/user/admin/program-management"
-          className="flex items-center gap-8 rounded-md border-2 bg-[#f3f6ff] p-4 md:text-lg"
-        >
-          <div className="relative w-[10rem]">
-            <Image src={programManage} alt="program management" />
-          </div>
-          <span>Manage programs and courses for all levels.</span>
         </Link>
       </div>
     </section>
