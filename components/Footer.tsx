c6deb4674a3096d12f3ffe063f8622fdfd32efcc
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-11-13 04:19:24 +0800

    Update footer text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="py-10 text-center text-sm font-medium">
-      &copy; Luis Cabantac
+      &copy; ScholaFlow. All rights reserved.
     </footer>
   );
 }
