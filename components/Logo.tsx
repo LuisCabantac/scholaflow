98f0d08936adb7e67966e394e54ed4611050c3d6
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-11-12 10:15:42 +0800

    Add showText prop to the logo component for showing/hiding the logo text

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -9,17 +9,19 @@ const quicksand: FontType = Quicksand({
   display: "swap",
 });
 
-export default function Logo() {
+export default function Logo({ showText = true }: { showText?: boolean }) {
   return (
     <div className="flex items-center justify-start gap-1">
       <div className="relative h-7 w-7 md:h-8 md:w-8">
         <Image src={scholaflowLogo} alt="logo" fill className="object-cover" />
       </div>
-      <p
-        className={`${quicksand.className} cursor-default text-xl font-bold text-[#22317c] md:text-2xl`}
-      >
-        ScholaFlow
-      </p>
+      {showText && (
+        <p
+          className={`${quicksand.className} cursor-default text-xl font-bold text-[#22317c] md:text-2xl`}
+        >
+          ScholaFlow
+        </p>
+      )}
     </div>
   );
 }
