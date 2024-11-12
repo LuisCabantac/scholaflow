df628905e5e1d89b773318d3488df5222adfc90a
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-11-13 07:12:58 +0800

    fix: Remove stroke current color property to the to-do sidebar icon causing it to change color when hovered

--- components/SidebarLinks.tsx
+++ components/SidebarLinks.tsx
@@ -134,11 +134,8 @@ export default function SidebarLinks({
                       onClick={() => !isMobile && handleSidebarExpand()}
                     >
                       <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth={2}
-                        stroke="currentColor"
                         className="size-6"
                       >
                         <path
