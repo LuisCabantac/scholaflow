569e4a4e0b03fd7ae195ade05f1f30d5f4487514
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-11-17 08:07:29 +0800

    Add copy link button to the ellipsis

--- components/EllipsisPopover.tsx
+++ components/EllipsisPopover.tsx
@@ -1,65 +1,65 @@
+import toast from "react-hot-toast";
+
 export default function EllipsisPopover({
   showEdit,
   deleteLabel,
   showEllipsis,
+  clipboardUrl,
+  showDelete = false,
   onShowEditForm,
+  onToggleEllipsis,
   onShowConfirmationScreen,
 }: {
   showEdit: boolean;
   deleteLabel?: string;
   showEllipsis: boolean;
+  showDelete?: boolean;
+  clipboardUrl?: string;
   onShowEditForm?: () => void;
-  onShowConfirmationScreen: () => void;
+  onToggleEllipsis: () => void;
+  onShowConfirmationScreen?: () => void;
 }) {
   return (
     <div
       className={`${showEllipsis ? "pointer-events-auto translate-y-0 opacity-100" : "pointer-events-none translate-y-[-10px] opacity-0"} ellipsis__popover absolute right-2 z-20 grid w-[10rem] gap-2 rounded-md bg-[#f3f6ff] p-3 font-medium shadow-md transition-all ease-in-out`}
     >
-      {showEdit && (
-        <div>
-          <button
-            type="button"
-            className="flex w-full items-center gap-1 rounded-md text-sm text-[#4c6ef5] hover:text-[#364fc7] md:text-base"
-            onClick={onShowEditForm}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="size-4"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-              />
-            </svg>
-            <span>Edit</span>
-          </button>
-        </div>
-      )}
-      <button
-        onClick={onShowConfirmationScreen}
-        className="flex items-center gap-1 rounded-md text-sm text-[#f03e3e] hover:text-[#c92a2a] md:text-base"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="currentColor"
-          className="size-4"
+      {clipboardUrl && (
+        <button
+          type="button"
+          className="flex w-full items-center gap-1 rounded-md text-sm hover:text-[#242628] md:text-base"
+          onClick={() => {
+            navigator.clipboard.writeText(clipboardUrl);
+            toast.success("Copied to clipboard!");
+            onToggleEllipsis();
+          }}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-          />
-        </svg>
-        <span>{deleteLabel ? deleteLabel : "Delete"}</span>
-      </button>
+          Copy link
+        </button>
+      )}
+      {showEdit && (
+        <button
+          type="button"
+          className="flex w-full items-center gap-1 rounded-md text-sm hover:text-[#242628] md:text-base"
+          onClick={() => {
+            onShowEditForm?.();
+            onToggleEllipsis();
+          }}
+        >
+          Edit
+        </button>
+      )}
+      {showDelete && (
+        <button
+          type="button"
+          onClick={() => {
+            onShowConfirmationScreen?.();
+            onToggleEllipsis();
+          }}
+          className="flex items-center gap-1 rounded-md text-sm text-[#f03e3e] hover:text-[#c92a2a] md:text-base"
+        >
+          {deleteLabel ? deleteLabel : "Delete"}
+        </button>
+      )}
     </div>
   );
 }
