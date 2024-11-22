81582cdad358b2984b8b7aed69e936cabe7331c9
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-11-22 17:06:18 +0800

    Add invite links
    - Allow users to send invite links for people to join classes

---
+++ app/join-class/[classCode]/page.tsx
@@ -0,0 +1,41 @@
+import { redirect } from "next/navigation";
+import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+
+import { auth } from "@/lib/auth";
+import { hasUser } from "@/lib/utils";
+
+import {
+  getClassByClassCode,
+  getEnrolledClassByClassAndSessionId,
+} from "@/lib/data-service";
+
+import Nav from "@/components/Nav";
+import InviteSection from "@/components/InviteSection";
+
+export default async function Page({ params }: { params: Params }) {
+  const session = await auth();
+
+  if (!hasUser(session)) return redirect("/signin");
+
+  const { classCode } = params;
+
+  const classroom = await getClassByClassCode(classCode);
+
+  if (!classroom) return redirect("/user/classroom");
+
+  const enrolledUser = await getEnrolledClassByClassAndSessionId(
+    classroom.classroomId,
+  );
+
+  if (enrolledUser || session.user.id === classroom.teacherId)
+    return redirect("/user/classroom");
+
+  return (
+    <div className="relative flex min-h-screen items-center justify-center bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(116,143,252,0.3),rgba(255,255,255,0))]">
+      <div className="absolute left-0 top-1.5 md:-top-0.5">
+        <Nav showButton={false} />
+      </div>
+      <InviteSection classroom={classroom} session={session} />
+    </div>
+  );
+}
---
+++ components/InviteSection.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
+import { ISession } from "@/lib/auth";
+import { joinClass } from "@/lib/classroom-actions";
+
+import { IClass } from "@/components/ClassroomSection";
+import SpinnerMini from "@/components/SpinnerMini";
+
+export default function InviteSection({
+  classroom,
+  session,
+}: {
+  classroom: IClass;
+  session: ISession;
+}) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccessful, setIsSuccessful] = useState(false);
+
+  async function handleJoinClass(event: React.FormEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+    const formData = new FormData(event.target as HTMLFormElement);
+    const classroomId = formData.get("classroomId") as string;
+    const { success, message } = await joinClass(classroomId);
+    if (success) {
+      setIsSuccessful(true);
+      setIsLoading(false);
+      router.push(`/user/classroom/class/${classroomId}`);
+    } else toast.error(message);
+  }
+
+  return (
+    <section className="z-10 mx-8 grid w-full gap-4 rounded-md border-2 border-[#dbe4ff] bg-[#f3f6ff] p-4 md:mx-60 md:w-[25rem] md:p-6">
+      <div className="w-full">
+        <h1 className="overflow-hidden text-ellipsis whitespace-nowrap text-lg font-semibold tracking-tight">
+          {classroom.className}
+        </h1>
+        <p className="overflow-hidden text-ellipsis whitespace-nowrap font-medium">
+          {classroom.subject && `${classroom.subject} · `}
+          {classroom.section}
+        </p>
+      </div>
+      <div className="flex gap-2 rounded-md border-2 border-[#dbe4ff] bg-[#f5f8ff] px-4 py-2">
+        <div className="relative h-8 w-8 rounded-full">
+          <Image
+            src={session.user.image}
+            alt={`${session.user.name}'s avatar`}
+            fill
+            className="rounded-full"
+          />
+        </div>
+        <div className="font-medium">
+          <p className="text-sm">{session.user.name}</p>
+          <p className="text-xs">{session.user.email}</p>
+        </div>
+      </div>
+      <form onSubmit={handleJoinClass}>
+        <input
+          type="text"
+          defaultValue={classroom.classroomId}
+          name="classroomId"
+          hidden
+        />
+
+        <button
+          className="flex h-10 w-full items-center justify-center gap-2 rounded-md bg-[#22317c] px-3 py-2 text-sm font-medium text-[#edf2ff] transition-colors hover:bg-[#384689] disabled:cursor-not-allowed disabled:bg-[#1b2763] disabled:text-[#d5dae6]"
+          disabled={isLoading || isSuccessful}
+          type="submit"
+        >
+          {isLoading && <SpinnerMini />}
+          <span>
+            {isSuccessful
+              ? "You've successfully joined the class!"
+              : "Join class"}
+          </span>
+        </button>
+      </form>
+    </section>
+  );
+}
