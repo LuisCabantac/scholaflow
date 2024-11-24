203b9624b54bec6d06102705822b2467eeeee753
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-11-24 13:57:28 +0800

    Remove icon from add user button in user management section

--- components/UserManagementSection.tsx
+++ components/UserManagementSection.tsx
@@ -77,21 +77,7 @@ export default function UserManagementSection({
             className="hidden rounded-md border-2 border-[#bec2cc] bg-[#f3f6ff] px-5 py-3 text-sm focus:border-[#384689] focus:outline-none md:inline md:w-48"
           />
           <Button type="primary" onClick={handleToggleShowUserForm}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="size-4 md:size-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
-              />
-            </svg>
-            <span>Add user</span>
+            Add user
           </Button>
         </div>
       </div>
