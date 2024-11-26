768ec41a604feb1e7c431d94fc9cd12b3d14f6ec
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-11-26 14:01:13 +0800

    Reduce hero and sign in page's background radial-gradient

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -21,7 +21,7 @@ export default async function Page() {
   if (session) return redirect("/user/classroom");
 
   return (
-    <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(237,242,255,0.3),rgba(255,255,255,0))]">
+    <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#f3f6ff] bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(116,143,252,0.1),rgba(255,255,255,0))]">
       <section className="flex items-center justify-center px-8 py-[8rem] md:grid md:grid-cols-[60%_40%] md:px-24 md:py-24">
         <div className="relative mx-0 my-auto w-0 md:w-[70%]">
           <Image
