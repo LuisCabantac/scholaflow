a9c1c9b9a20dc07b210af10998ed22865b94b2d8
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-11-26 14:01:56 +0800

    Reduce sign in page's welcome back text line height

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -21,7 +21,7 @@ export default async function Page() {
   if (session) return redirect("/user/classroom");
 
   return (
-    <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#f3f6ff] bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(116,143,252,0.1),rgba(255,255,255,0))]">
+    <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(237,242,255,0.3),rgba(255,255,255,0))]">
       <section className="flex items-center justify-center px-8 py-[8rem] md:grid md:grid-cols-[60%_40%] md:px-24 md:py-24">
         <div className="relative mx-0 my-auto w-0 md:w-[70%]">
           <Image
@@ -32,7 +32,9 @@ export default async function Page() {
         </div>
         <div className="flex w-full flex-col justify-center gap-10">
           <Logo />
-          <h2 className="text-3xl font-semibold md:text-5xl">Welcome back!</h2>
+          <h2 className="text-3xl font-semibold tracking-tighter md:text-5xl">
+            Welcome back!
+          </h2>
           <div className="grid gap-3">
             <SignInForm />
             <SignInGoogleButton />
