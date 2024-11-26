c293ddce605a0853f21c5796ccbc49202ccead7d
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-11-27 05:22:23 +0800

    Add back button to the sign in and sign up pages for mobile screens

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -23,7 +23,7 @@ export default async function Page() {
 
   return (
     <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#f3f6ff] bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(116,143,252,0.2),rgba(255,255,255,0))]">
-      <section className="flex items-center justify-center px-8 py-[8rem] md:grid md:grid-cols-[55%_45%] md:px-24 md:py-14">
+      <section className="relative flex items-center justify-center px-8 py-[8rem] md:grid md:grid-cols-[55%_45%] md:px-24 md:py-14">
         <div className="relative mx-0 my-auto w-0 md:w-[70%]">
           <Image
             src={signUpPageImage}
@@ -31,6 +31,22 @@ export default async function Page() {
             className="object-cover"
           />
         </div>
+        <Link href="/" className="absolute left-6 top-6 md:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+            />
+          </svg>
+        </Link>
         <div className="flex w-full flex-col justify-center gap-10">
           <Logo />
           <h2 className="text-3xl font-semibold tracking-tighter md:text-5xl">
