d4ba8ca315a7bc4da182435673cc511492b33f2d
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-11-28 09:54:09 +0800

    Reduce close button in the stream submission for mobile screens

--- components/StreamSubmissionsSection.tsx
+++ components/StreamSubmissionsSection.tsx
@@ -460,7 +460,7 @@ export default function StreamSubmissionsSection({
                   viewBox="0 0 24 24"
                   strokeWidth={2}
                   stroke="currentColor"
-                  className="size-6"
+                  className="size-5"
                 >
                   <path
                     strokeLinecap="round"
