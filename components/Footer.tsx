745982f9a164a58ff2f243297be055de8f068b5b
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-12-01 07:44:55 +0800

    Switch footer text places

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="flex items-center justify-between border-t-2 border-[#dbe4ff] px-4 py-4 font-medium md:px-8">
-      <Link href="/privacy" className="hover:underline">
-        Privacy
-      </Link>
       <p>&copy; 2024 ScholaFlow</p>
+      <Link href="/privacy" className="hover:underline">
+        Privacy Policy
+      </Link>
     </footer>
   );
 }
