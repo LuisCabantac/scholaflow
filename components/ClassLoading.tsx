48bbe92752de513d5721ee854ba0ae7a71fe3bf0
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-12-04 14:12:02 +0800

    Update comment's skeleton loading color
    - Reduce class skeleton loading's title width

--- components/ClassLoading.tsx
+++ components/ClassLoading.tsx
@@ -3,7 +3,7 @@ export default function ClassLoading() {
     <ul className="contents">
       <li className="relative flex h-[9rem] w-full flex-col gap-2 rounded-md border border-[#dddfe6] bg-[#f3f6ff] p-4 shadow-sm md:h-[10rem]">
         <div className="absolute left-3 top-3 grid w-[90%] gap-2 md:left-4 md:top-4">
-          <div className="h-[1.125rem] w-full animate-pulse rounded-md bg-[#e0e7ff]"></div>
+          <div className="h-[1.125rem] w-[90%] animate-pulse rounded-md bg-[#e0e7ff]"></div>
           <div className="h-[0.875rem] w-16 animate-pulse rounded-md bg-[#e0e7ff]"></div>
         </div>
         <div className="absolute bottom-3 left-3 flex items-center gap-2 md:bottom-4 md:left-4">
@@ -16,7 +16,7 @@ export default function ClassLoading() {
       </li>
       <li className="relative flex h-[9rem] w-full flex-col gap-2 rounded-md border border-[#dddfe6] bg-[#f3f6ff] p-4 shadow-sm md:h-[10rem]">
         <div className="absolute left-3 top-3 grid w-[90%] gap-2 md:left-4 md:top-4">
-          <div className="h-[1.125rem] w-full animate-pulse rounded-md bg-[#e0e7ff]"></div>
+          <div className="h-[1.125rem] w-[90%] animate-pulse rounded-md bg-[#e0e7ff]"></div>
           <div className="h-[0.875rem] w-16 animate-pulse rounded-md bg-[#e0e7ff]"></div>
         </div>
         <div className="absolute bottom-3 left-3 flex items-center gap-2 md:bottom-4 md:left-4">
--- components/CommentsLoading.tsx
+++ components/CommentsLoading.tsx
@@ -1,13 +1,10 @@
 export default function CommentsLoading() {
   return (
     <li className="flex gap-2">
-      <div className="h-8 w-8 animate-pulse rounded-full bg-[#dbe4ff]"></div>
+      <div className="h-8 w-8 animate-pulse rounded-full bg-[#e0e7ff]"></div>
       <div className="grid gap-2">
-        <div className="flex gap-1">
-          <div className="h-[0.875rem] w-20 animate-pulse rounded-md bg-[#dbe4ff]"></div>
-          <div className="h-[0.75rem] w-12 animate-pulse rounded-md bg-[#dbe4ff]"></div>
-        </div>
-        <div className="h-[0.875rem] w-28 animate-pulse rounded-md bg-[#dbe4ff]"></div>
+        <div className="h-[0.875rem] w-20 animate-pulse rounded-md bg-[#e0e7ff]"></div>
+        <div className="h-[0.875rem] w-28 animate-pulse rounded-md bg-[#e0e7ff]"></div>
       </div>
     </li>
   );
