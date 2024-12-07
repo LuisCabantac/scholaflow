b8ecb610410c72fc6ab0618a0320aa598fbc5e0d
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-12-07 18:41:11 +0800

    Add user info labels back in the profile section skeleton

--- app/user/profile/loading.tsx
+++ app/user/profile/loading.tsx
@@ -15,18 +15,18 @@ export default function Loading() {
         <div className="flex flex-col items-start justify-start gap-2 md:flex-row md:items-center md:justify-between">
           <div className="mt-0.5 grid gap-2 md:flex md:items-center">
             <div>
-              <div className="mb-1.5 h-[0.875rem] w-48 animate-pulse rounded-md bg-[#dbe4ff]"></div>
-              <div className="h-[0.75rem] w-10 animate-pulse rounded-md bg-[#dbe4ff]"></div>
+              <div className="mb-1 h-[0.875rem] w-48 animate-pulse rounded-md bg-[#dbe4ff]"></div>
+              <h4 className="text-xs font-medium text-[#616572]">Email</h4>
             </div>
             <div className="mx-4 hidden h-8 w-px bg-[#dbe4ff] md:block"></div>
             <div>
-              <div className="mb-1.5 h-[0.875rem] w-20 animate-pulse rounded-md bg-[#dbe4ff]"></div>
-              <div className="h-[0.75rem] w-12 animate-pulse rounded-md bg-[#dbe4ff]"></div>
+              <div className="mb-1 h-[0.875rem] w-20 animate-pulse rounded-md bg-[#dbe4ff]"></div>
+              <h4 className="text-xs font-medium text-[#616572]">School</h4>
             </div>
             <div className="mx-4 hidden h-8 w-px bg-[#dbe4ff] md:block"></div>
             <div>
-              <div className="mb-1.5 h-[0.875rem] w-24 animate-pulse rounded-md bg-[#dbe4ff]"></div>
-              <div className="h-[0.75rem] w-12 animate-pulse rounded-md bg-[#dbe4ff]"></div>
+              <div className="mb-1 h-[0.875rem] w-24 animate-pulse rounded-md bg-[#dbe4ff]"></div>
+              <h4 className="text-xs font-medium text-[#616572]">Joined</h4>
             </div>
           </div>
         </div>
