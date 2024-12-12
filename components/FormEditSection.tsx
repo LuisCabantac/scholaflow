82a981090769fe76591dd4dd49c054d8d7847f10
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-12-13 06:35:47 +0800

    Add questions when the Add question button is clicked
    - Set the active question when a question card is clicked.
    - Add styles to the long and short answer input fields.

--- components/FormEditSection.tsx
+++ components/FormEditSection.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { v4 as uuid } from "uuid";
+import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 import Button from "@/components/Button";
-import { IForm } from "@/components/FormsSection";
 import FormOptions from "@/components/FormOptions";
+import { IForm, IQuestionSet } from "@/components/FormsSection";
 
 export default function FormEditSection({
   formId,
@@ -13,14 +15,34 @@ export default function FormEditSection({
   formId: string;
   onGetForm: (formId: string) => Promise<IForm | null>;
 }) {
+  const [activeQuestion, setActiveQuestion] = useState<string | null>(null);
+
   const { data: form } = useQuery({
     queryKey: [`form--${formId}--edit`],
     queryFn: () => onGetForm(formId),
   });
 
+  const [questionSet, setQuestionSet] = useState<IQuestionSet[]>(
+    form?.questionSet ?? [],
+  );
+
+  function handleQuestionChange(
+    event: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >,
+  ) {
+    setQuestionSet((prevQuestion) =>
+      prevQuestion.map((question) =>
+        question.id === activeQuestion
+          ? { ...question, [event.target.name]: event.target.value }
+          : question,
+      ),
+    );
+  }
+
   return (
     <section className="grid grid-cols-[1fr_20rem] gap-4">
-      <ul className="grid gap-2">
+      <ul className="flex flex-col gap-2">
         <li className="rounded-md border border-[#dddfe6] bg-[#f3f6ff] p-3 shadow-sm md:p-4">
           <form className="grid gap-2">
             <div className="flex items-center justify-between gap-2">
@@ -32,7 +54,6 @@ export default function FormEditSection({
               />
               <Button type="primary">Publish</Button>
             </div>
-
             <input
               type="text"
               className="w-full border-none bg-transparent"
@@ -41,61 +62,111 @@ export default function FormEditSection({
             />
           </form>
         </li>
-        {form?.questionSet.map((form) => (
+        {questionSet.map((question) => (
           <li
-            key={form.id}
-            className="rounded-md border border-[#dddfe6] bg-[#f3f6ff] p-3 shadow-sm md:p-4"
+            key={question.id}
+            className={`rounded-md border border-[#dddfe6] bg-[#f3f6ff] p-3 shadow-sm md:p-4 ${activeQuestion !== question.id && "cursor-pointer"}`}
+            onClick={() =>
+              activeQuestion !== question.id && setActiveQuestion(question.id)
+            }
           >
             <form className="grid gap-2">
               <input
                 type="text"
-                name={`question-${form.id}`}
-                defaultValue={form.question}
+                name="question"
+                defaultValue={question.question}
                 className="w-full border-none bg-transparent font-medium"
+                onChange={handleQuestionChange}
               />
-              <FormOptions optionType={form.type} options={form.options} />
-              <button className="flex items-center gap-2 font-medium text-[#5c7cfa]">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  className="size-3"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <circle cx="12" cy="12" r="10" />
-                  <path d="M8 12h8" />
-                  <path d="M12 8v8" />
-                </svg>
-                Add more options
-              </button>
+              <FormOptions
+                onQuestionChange={handleQuestionChange}
+                options={question.options}
+                optionType={question.type}
+              />
+              {question.type !== "paragraph" &&
+                question.type !== "shortAnswer" && (
+                  <button
+                    type="button"
+                    className="flex items-center gap-2 font-medium text-[#5c7cfa]"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      className="size-3"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <circle cx="12" cy="12" r="10" />
+                      <path d="M8 12h8" />
+                      <path d="M12 8v8" />
+                    </svg>
+                    Add more options
+                  </button>
+                )}
             </form>
-            <div className="mt-2 flex items-center justify-between border-t pt-2">
-              <div className="flex items-center gap-2">
-                <p className="font-medium">Question Type: </p>
-                <select className="rounded-md border bg-transparent px-2 py-1">
-                  <option>Multiple choice</option>
-                  <option>Checkboxes</option>
-                  <option>Dropdown</option>
-                  <option>Paragraph</option>
-                  <option>Short answer</option>
-                </select>
+            {activeQuestion === question.id && (
+              <div className="mt-2 flex items-center justify-between border-t pt-2">
+                <div className="flex items-center gap-2">
+                  <p className="font-medium">Question Type: </p>
+                  <select
+                    className="rounded-md border bg-transparent px-2 py-1"
+                    defaultValue={question.type}
+                    name="type"
+                    onChange={handleQuestionChange}
+                  >
+                    <option value="multipleChoice">Multiple choice</option>
+                    <option value="checkboxes">Checkboxes</option>
+                    <option value="dropdown">Dropdown</option>
+                    <option value="paragraph">Paragraph</option>
+                    <option value="shortAnswer">Short answer</option>
+                  </select>
+                </div>
+                <div className="flex items-center gap-2">
+                  <label className="flex items-center gap-2">
+                    Required
+                    <input type="checkbox" className="accent-[#5c7cfa]" />
+                  </label>
+                  <div className="h-4 w-[1px] bg-[#dddfe6]"></div>
+                  <button
+                    type="button"
+                    className="text-[#f03e3e] hover:text-[#c92a2a]"
+                    onClick={() => {
+                      if (question.id === activeQuestion)
+                        setActiveQuestion(null);
+                      setQuestionSet(
+                        questionSet.filter(
+                          (prevQuestion) => prevQuestion.id !== question.id,
+                        ),
+                      );
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
-              <div className="flex items-center gap-2">
-                <label className="flex items-center gap-2">
-                  Required
-                  <input type="checkbox" className="accent-[#5c7cfa]" />
-                </label>
-                <div className="h-4 w-[1px] bg-[#dddfe6]"></div>
-                <p className="text-[#f03e3e] hover:text-[#c92a2a]">Delete</p>
-              </div>
-            </div>
+            )}
           </li>
         ))}
         <li className="flex items-center justify-center">
-          <Button type="secondary">Add question</Button>
+          <Button
+            type="secondary"
+            onClick={() =>
+              setQuestionSet([
+                ...questionSet,
+                {
+                  id: uuid(),
+                  type: "shortAnswer",
+                  question: "Untitled Question",
+                  required: false,
+                },
+              ])
+            }
+          >
+            Add question
+          </Button>
         </li>
       </ul>
       <div className="max-h-screen rounded-md border border-[#dddfe6] bg-[#f3f6ff] p-3 shadow-sm md:p-4">
--- components/FormOptions.tsx
+++ components/FormOptions.tsx
@@ -3,9 +3,15 @@ import { QuestionType } from "@/components/FormsSection";
 export default function FormOptions({
   options,
   optionType,
+  onQuestionChange,
 }: {
   options: string[] | undefined;
   optionType: QuestionType;
+  onQuestionChange: (
+    event: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >,
+  ) => void;
 }) {
   if (optionType === "checkboxes" && options)
     return (
@@ -32,8 +38,23 @@ export default function FormOptions({
         ))}
       </>
     );
-  if (optionType === "shortAnswer") return <input type="text" />;
-  if (optionType === "paragraph") return <textarea></textarea>;
+  if (optionType === "shortAnswer")
+    return (
+      <input
+        type="text"
+        disabled
+        className="w-full rounded-md border border-[#dddfe6] bg-transparent px-4 py-2 placeholder:text-[#616572] focus:border-[#384689] focus:outline-none disabled:cursor-not-allowed"
+        placeholder="Short answer text"
+      />
+    );
+  if (optionType === "paragraph")
+    return (
+      <textarea
+        disabled
+        className="h-10 w-full rounded-md border border-[#dddfe6] bg-transparent px-4 py-2 placeholder:text-[#616572] focus:border-[#384689] focus:outline-none disabled:cursor-not-allowed"
+        placeholder="Long answer text"
+      ></textarea>
+    );
   if (optionType === "dropdown" && options)
     return (
       <select>
