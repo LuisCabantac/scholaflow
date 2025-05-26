d27bcfe881e7b0c9de4602b1d037517f4bad5be0
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-05-26 08:37:51 +0800

    feat: Add better-auth, drizzle-orm, pg, postgres, and drizzle-kit

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,114 +1,26 @@
-import NextAuth, { Account, Session, User } from "next-auth";
-import { JWT } from "next-auth/jwt";
-import { AdapterAccount, AdapterUser } from "next-auth/adapters";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/drizzle/index";
+import { createAuthMiddleware } from "better-auth/api";
 
-import { createUser } from "@/lib/auth-actions";
-import { getUser, getUserByEmail } from "@/lib/data-service";
-import { generatePassword } from "@/lib/utils";
-
-export interface ISession {
-  user: {
-    id: string;
-    name: string;
-    email: string;
-    image: string;
-    role: "student" | "teacher" | "admin";
-  };
-  expires: string;
-}
-
-interface IUser extends User {
-  id: string;
-  name: string;
-  email: string;
-  image: string;
-  role?: "student" | "teacher" | "admin";
-}
-
-const authConfig = {
-  session: {
-    strategy: "jwt" as const,
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: true,
   },
-  providers: [
-    CredentialsProvider({
-      async authorize(credentials: Partial<Record<string, unknown>>) {
-        if (!credentials.email) return null;
-        const email = credentials.email as string;
-        const user = await getUserByEmail(email);
-        if (user && user.password === credentials.password) {
-          return user;
-        } else {
-          return null;
-        }
-      },
-    }),
-    GoogleProvider({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-    }),
-  ],
-  callbacks: {
-    async signIn({
-      user,
-      account,
-    }: {
-      user: User | AdapterUser;
-      account: Account | AdapterAccount | null;
-    }) {
-      try {
-        const existingUser = await getUserByEmail((user as IUser).email);
-        if (
-          !existingUser?.emailVerified &&
-          account?.provider === "credentials"
-        ) {
-          return false;
-        }
-        if (!existingUser) {
-          await createUser({
-            email: user.email,
-            fullName: user.name,
-            avatar: user.image,
-            role: "student",
-            emailVerified: true,
-            password: generatePassword(8),
-          });
-        }
-        return true;
-      } catch {
-        return false;
+  socialProviders: {
+    google: {
+      clientId: process.env.AUTH_GOOGLE_ID ?? "",
+      clientSecret: process.env.AUTH_GOOGLE_SECRET ?? "",
+    },
+  },
+  hooks: {
+    before: createAuthMiddleware(async (ctx) => {
+      if (ctx.body) {
+        console.log(ctx.body);
       }
-    },
-    async jwt({ token, user }: { token: JWT; user?: User | AdapterUser }) {
-      if (user) token.role = (user as IUser).role;
-      return token;
-    },
-    async session({ session }: { session: Session }) {
-      const user = await getUser((session as ISession).user.email);
-
-      (session as ISession).user.id = user.id;
-      (session as ISession).user.name = user.fullName;
-      (session as ISession).user.image = user.avatar;
-      (session as ISession).user.role = user.role;
-      return session;
-    },
+    }),
   },
-  pages: {
-    signIn: "/signin",
-  },
-};
-
-export const {
-  auth,
-  signIn,
-  signOut,
-  handlers: { GET, POST },
-} = NextAuth(authConfig);
+});
