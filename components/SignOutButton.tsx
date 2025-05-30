a76c9356b91b4b3af35f08c25f845f18688183de
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-05-31 06:02:24 +0800

    feat: Integrate Google provider with better-auth

--- components/SignOutButton.tsx
+++ components/SignOutButton.tsx
@@ -1,23 +1,37 @@
-import { signOutAction } from "@/lib/auth-actions";
+"use client";
+
+import toast from "react-hot-toast";
+
+import { signOut } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 export default function SignOutButton() {
+  const router = useRouter();
+
   return (
-    <form action={signOutAction}>
-      <button
-        className="signout__button flex w-full gap-2 rounded-md px-2 py-2 transition-colors hover:bg-[#d8e0f5]"
-        type="submit"
+    <button
+      className="signout__button flex w-full gap-2 rounded-md px-2 py-2 transition-colors hover:bg-[#d8e0f5]"
+      onClick={async () => {
+        await signOut({
+          fetchOptions: {
+            onSuccess: () => {
+              router.push("/signin");
+              toast.success("You've been signed out successfully");
+            },
+          },
+        });
+      }}
+    >
+      <svg
+        viewBox="0 0 24 24"
+        strokeWidth={2}
+        className="size-5 fill-transparent stroke-[#5c7cfa] transition-colors"
       >
-        <svg
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          className="size-5 fill-transparent stroke-[#5c7cfa] transition-colors"
-        >
-          <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
-          <path d="M9 12h12l-3 -3" />
-          <path d="M18 15l3 -3" />
-        </svg>
-        <span>Sign out</span>
-      </button>
-    </form>
+        <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+        <path d="M9 12h12l-3 -3" />
+        <path d="M18 15l3 -3" />
+      </svg>
+      <span>Sign out</span>
+    </button>
   );
 }
