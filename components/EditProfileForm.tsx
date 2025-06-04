741ca7457adf1afb0f6531a60d5dfeae66163fc7
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-06-05 07:12:03 +0800

    refactor: Remove unused password fields and related logic from EditProfileForm

--- components/EditProfileForm.tsx
+++ components/EditProfileForm.tsx
@@ -35,8 +35,6 @@ export default function EditProfileForm({
   const [attachmentPreview, setAttachmentPreview] = useState<string>(
     user?.image ?? "",
   );
-  // const [showPassword, setShowPassword] = useState(false);
-  // const [validPassword, setValidPassword] = useState(true);
   const [showConfirmation, setShowConfirmation] = useState(false);
 
   async function handleSubmitEditProfile(event: React.FormEvent) {
@@ -77,11 +75,6 @@ export default function EditProfileForm({
       reader.readAsDataURL(selectedFile);
     }
   }
-
-  // function handleShowPassword(event: React.MouseEvent<HTMLButtonElement>) {
-  //   event.preventDefault();
-  //   setShowPassword(!showPassword);
-  // }
 
   function handleToggleShowConfirmation() {
     setShowConfirmation(!showConfirmation);
@@ -169,7 +162,7 @@ export default function EditProfileForm({
               <input
                 required
                 disabled={isLoading}
-                name="fullName"
+                name="name"
                 type="text"
                 defaultValue={user?.name ?? ""}
                 placeholder="Add your full name..."
@@ -201,58 +194,6 @@ export default function EditProfileForm({
                 className="rounded-md border border-[#dddfe6] bg-transparent px-4 py-2 placeholder:text-[#616572] focus:border-[#384689] focus:outline-none disabled:cursor-not-allowed disabled:text-[#616572]"
               />
             </div>
-            {/* <div className="grid gap-2">
-              <label className="font-medium">
-                Password <span className="text-red-400">*</span>
-              </label>
-              <div className="flex">
-                <input
-                  required
-                  name="password"
-                  type={showPassword ? "text" : "password"}
-                  placeholder="Set up the user's password"
-                  disabled={isLoading}
-                  className={`password__input rounded-y-md w-full rounded-l-md border-y border-l border-[#dddfe6] bg-transparent px-4 py-2 focus:border-[#384689] focus:outline-none ${!validPassword && "border-[#f03e3e]"}`}
-                  defaultValue={user?.password}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                    setValidPassword(
-                      event.target.value.length >= 8 ? true : false,
-                    )
-                  }
-                />
-                <button
-                  onClick={handleShowPassword}
-                  className={`show__password rounded-r-md border-y border-r border-[#dddfe6] py-2 pr-4 focus:outline-0 ${!validPassword && "border-[#f03e3e]"}`}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={2}
-                    className="size-6 stroke-[#616572]"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-                    />
-                    {showPassword ? (
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
-                      />
-                    ) : (
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                      />
-                    )}
-                  </svg>
-                </button>
-              </div>
-            </div> */}
           </div>
           <div className="fixed bottom-0 left-0 right-0 flex w-auto items-center justify-between gap-2 border-t border-[#dddfe6] bg-[#f3f6ff] px-4 py-4 md:px-8">
             <button
