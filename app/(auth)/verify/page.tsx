20b6e02be837b7ffb796aac565df81ce5e636af8
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-06-08 04:25:02 +0800

    refactor: Enhance email verification page with logo, image, and layout

--- app/(auth)/verify/page.tsx
+++ app/(auth)/verify/page.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
+import Image from "next/image";
 import { Metadata } from "next";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { auth } from "@/lib/auth";
+import verifyEmailPageImage from "@/public/landing_page/verify-email.svg";
 
-import Nav from "@/components/Nav";
+import Logo from "@/components/Logo";
 import VerifySection from "@/components/VerifySection";
 
 export const metadata: Metadata = {
@@ -21,11 +24,41 @@ export default async function Page() {
   if (session) return redirect("/classroom");
 
   return (
-    <div className="relative flex min-h-screen items-center justify-center bg-[#f3f6ff] bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(116,143,252,0.2),rgba(255,255,255,0))]">
-      <div className="absolute left-0 top-1.5 md:-top-0.5">
-        <Nav showButton={false} />
-      </div>
-      <VerifySection />
+    <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#f3f6ff] bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(116,143,252,0.2),rgba(255,255,255,0))]">
+      <section className="relative flex items-center justify-center px-8 py-[8rem] md:grid md:h-full md:grid-cols-[55%_45%] md:px-24 md:py-0">
+        <div className="relative mx-0 my-auto w-0 md:w-[70%]">
+          <Image
+            src={verifyEmailPageImage}
+            alt="road to knowledge"
+            className="select-none object-cover"
+          />
+        </div>
+        <Link href="/" className="absolute left-6 top-6 md:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+            />
+          </svg>
+        </Link>
+        <div className="flex w-full flex-col justify-center gap-10">
+          <Logo />
+          <h2 className="text-3xl font-semibold tracking-tighter md:text-5xl">
+            Verify your email
+          </h2>
+          <div className="grid gap-3">
+            <VerifySection />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- components/VerifySection.tsx
+++ components/VerifySection.tsx
@@ -5,6 +5,8 @@ import { useRouter, useSearchParams } from "next/navigation";
 
 import { verifyEmailVerification } from "@/lib/auth-actions";
 
+import SpinnerMini from "@/components/SpinnerMini";
+
 export default function VerifySection() {
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -35,53 +37,62 @@ export default function VerifySection() {
   }, [onSubmit]);
 
   return (
-    <section className="z-10 mx-8 grid w-full gap-2 rounded-md border border-[#dddfe6] bg-[#f3f6ff] p-4 md:mx-60 md:w-[25rem]">
-      <h1 className="overflow-hidden text-ellipsis whitespace-nowrap text-lg font-semibold tracking-tight">
-        Verify your email
-      </h1>
-      {isLoading && (
-        <div className="flex items-center gap-2">
-          <div className="spinner__mini dark"></div> Verifying...
-        </div>
-      )}
-      {success && (
-        <div className="flex gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="size-5 flex-shrink-0 stroke-[#37b24d]"
+    <div className="grid gap-3 pb-3">
+      <div className="grid gap-2">
+        {isLoading && (
+          <div className="flex h-10 w-full items-center justify-center gap-2 rounded-md border border-[#dddfe6] bg-[#f3f6ff] px-4 py-2">
+            <SpinnerMini />
+            <span className="text-sm font-medium">Verifying...</span>
+          </div>
+        )}
+        {success && (
+          <div className="text-muted-foreground flex gap-2 rounded-md border p-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="mt-0.5 size-6 flex-shrink-0 stroke-[#37b24d]"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z"
+              />
+            </svg>
+            <span>{message}</span>
+          </div>
+        )}
+        {success === false && success !== null && (
+          <div className="text-muted-foreground flex gap-2 rounded-md border p-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="mt-0.5 size-6 flex-shrink-0 stroke-[#f03e3e]"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            <span>{message}</span>
+          </div>
+        )}
+        {success === false && (
+          <button
+            className="flex h-10 w-full items-center justify-center gap-2 rounded-md bg-[#22317c] px-4 py-2 text-sm font-medium text-[#edf2ff] transition-colors hover:bg-[#384689]"
+            onClick={() => router.push("/")}
+            type="button"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z"
-            />
-          </svg>
-          <span>{message}</span>
-        </div>
-      )}
-      {success === false && success !== null && (
-        <div className="flex gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="size-5 flex-shrink-0 stroke-[#f03e3e]"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
-          <span>{message}</span>
-        </div>
-      )}
-    </section>
+            Go Back to Home
+          </button>
+        )}
+      </div>
+    </div>
   );
 }
