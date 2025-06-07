0a212f29183287d7af4a5e6daf04ee3cf10741bf
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-06-08 04:12:00 +0800

    fix: Increase vertical padding on reset and forget password sections

--- app/(auth)/forget-password/page.tsx
+++ app/(auth)/forget-password/page.tsx
@@ -25,7 +25,7 @@ export default async function Page() {
 
   return (
     <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#f3f6ff] bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(116,143,252,0.2),rgba(255,255,255,0))]">
-      <section className="relative flex items-center justify-center px-8 py-[7rem] md:grid md:h-full md:grid-cols-[55%_45%] md:px-24 md:py-0">
+      <section className="relative flex items-center justify-center px-8 py-[8rem] md:grid md:h-full md:grid-cols-[55%_45%] md:px-24 md:py-0">
         <div className="relative mx-0 my-auto w-0 md:w-[70%]">
           <Image
             src={forgetPasswordPageImage}
