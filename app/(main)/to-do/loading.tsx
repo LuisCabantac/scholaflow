4b892d2405116b688f4986028779a79d9ca06ab4
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-06-10 17:29:57 +0800

    refactor: Update to do section and loading components

--- app/(main)/to-do/loading.tsx
+++ app/(main)/to-do/loading.tsx
@@ -1,28 +1,30 @@
-import SkeletonTabsStatic from "@/components/SkeletonTabsStatic";
-
 export default function Loading() {
   return (
     <div className="flex flex-col items-start justify-start" role="status">
       <span className="sr-only">Loading…</span>
-      <SkeletonTabsStatic skeletonRoute="to-do" />
+      <div className="flex gap-1 rounded-lg bg-muted p-1">
+        <div className="h-6 w-16 animate-pulse rounded-md bg-muted-foreground/20"></div>
+        <div className="h-6 w-20 animate-pulse rounded-md bg-muted-foreground/20"></div>
+        <div className="h-6 w-20 animate-pulse rounded-md bg-muted-foreground/20"></div>
+      </div>
       <ul className="mt-2 grid w-full gap-2">
         {Array(6)
           .fill(undefined)
           .map((_, index) => (
             <li
               key={index}
-              className="flex items-center justify-between gap-2 rounded-md border border-[#dddfe6] bg-[#f3f6ff] p-3 shadow-sm md:p-4"
+              className="flex items-center justify-between gap-2 rounded-md border bg-card p-3 shadow-sm md:p-4"
             >
               <div className="flex gap-2">
-                <div className="size-6 animate-pulse rounded-md bg-[#e0e7ff]"></div>
+                <div className="size-6 animate-pulse rounded-md bg-muted"></div>
                 <div className="grid gap-2">
-                  <div className="h-[0.875rem] w-36 animate-pulse rounded-md bg-[#e0e7ff]"></div>
-                  <div className="h-[0.875rem] w-24 animate-pulse rounded-md bg-[#e0e7ff]"></div>
-                  <div className="mt-2 h-[0.75rem] w-16 animate-pulse rounded-md bg-[#e0e7ff]"></div>
-                  <div className="block h-[0.75rem] w-14 animate-pulse rounded-md bg-[#e0e7ff] md:hidden"></div>
+                  <div className="h-[0.875rem] w-36 animate-pulse rounded-md bg-muted"></div>
+                  <div className="h-[0.875rem] w-24 animate-pulse rounded-md bg-muted"></div>
+                  <div className="mt-2 h-[0.75rem] w-16 animate-pulse rounded-md bg-muted"></div>
+                  <div className="block h-[0.75rem] w-14 animate-pulse rounded-md bg-muted md:hidden"></div>
                 </div>
               </div>
-              <div className="hidden h-[0.875rem] w-16 animate-pulse rounded-md bg-[#e0e7ff] md:block"></div>
+              <div className="hidden h-[0.875rem] w-16 animate-pulse rounded-md bg-muted md:block"></div>
             </li>
           ))}
       </ul>
