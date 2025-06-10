3062aab0a42c19e64f48b478d834377dbafab811
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-06-10 10:26:03 +0800

    refactor: Redirect signed in users to classroom from landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 import { Metadata } from "next";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
 
 import Nav from "@/components/Nav";
 import LandingPage from "@/components/LandingPage";
@@ -6,11 +10,18 @@ import LandingPage from "@/components/LandingPage";
 export const metadata: Metadata = {
   title: "ScholaFlow - Your All-in-One Learning Platform",
 };
-export default function Home() {
+
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) return redirect("/classroom");
+
   return (
-    <div className="relative min-h-screen bg-[#f3f6ff] bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(116,143,252,0.2),rgba(255,255,255,0))]">
+    <>
       <Nav showButton={true} />
       <LandingPage />
-    </div>
+    </>
   );
 }
