614a8edfcbbe469f0355748d9c84b92437b447e8
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-06-13 12:27:42 +0800

    refactor: Update EllipsisPopover component styling

--- components/EllipsisPopover.tsx
+++ components/EllipsisPopover.tsx
@@ -21,12 +21,12 @@ export default function EllipsisPopover({
 }) {
   return (
     <div
-      className={`${showEllipsis ? "pointer-events-auto translate-y-0 opacity-100" : "pointer-events-none translate-y-[-10px] opacity-0"} ellipsis__popover absolute right-2 z-20 grid w-[10rem] gap-2 rounded-md bg-[#f3f6ff] p-3 font-medium shadow-md transition-all ease-in-out`}
+      className={`${showEllipsis ? "pointer-events-auto translate-y-0 opacity-100" : "pointer-events-none translate-y-[-10px] opacity-0"} ellipsis__popover absolute right-2 z-20 grid w-[10rem] gap-2 rounded-xl border bg-card p-3 font-medium shadow-md transition-all ease-in-out`}
     >
       {clipboardUrl && (
         <button
           type="button"
-          className="flex w-full items-center gap-1 rounded-md hover:text-[#242628]"
+          className="flex w-full items-center gap-1 rounded-md hover:text-foreground"
           onClick={async () => {
             await navigator.clipboard.writeText(clipboardUrl).then(() => {
               toast.success("Copied to clipboard!");
@@ -40,7 +40,7 @@ export default function EllipsisPopover({
       {showEdit && (
         <button
           type="button"
-          className="flex w-full items-center gap-1 rounded-md hover:text-[#242628]"
+          className="flex w-full items-center gap-1 rounded-md hover:text-foreground"
           onClick={() => {
             onShowEditForm?.();
             onToggleEllipsis();
@@ -56,7 +56,7 @@ export default function EllipsisPopover({
             onShowConfirmationModal?.();
             onToggleEllipsis();
           }}
-          className="flex items-center gap-1 rounded-md text-[#f03e3e] hover:text-[#c92a2a]"
+          className="flex items-center gap-1 rounded-md text-destructive hover:text-destructive/90"
         >
           {deleteLabel ? deleteLabel : "Delete"}
         </button>
