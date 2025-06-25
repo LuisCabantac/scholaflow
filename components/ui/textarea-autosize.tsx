8769828840bd28a900d57937a9472db90f30e3d1
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-06-26 07:54:14 +0800

    chore: Update textarea border radius from rounded-md to rounded-xl

--- components/ui/textarea-autosize.tsx
+++ components/ui/textarea-autosize.tsx
@@ -3,7 +3,7 @@ import TextareaAutosizeComponent from "react-textarea-autosize";
 import { cn } from "@/lib/utils";
 
 const textareaStyles =
-  "flex min-h-10 w-full rounded-md border border-input bg-foreground/10 px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm";
+  "flex min-h-10 w-full rounded-xl border border-input bg-foreground/10 px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm";
 
 function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
   return (
