aa677e1c45c563d79e6dac2a05bd758f4ef19d0e
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2026-09-15 13:22:27 +0800

    chore: update footer component format

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,21 +2,21 @@ import { Link } from "@tanstack/react-router";
 import { useTranslation } from "react-i18next";
 
 export default function Footer() {
-	const { t } = useTranslation("common");
+  const { t } = useTranslation("common");
 
-	return (
-		<footer className="flex flex-col items-center gap-4 border-t px-4 py-4 text-xs text-foreground/70 md:flex-row md:justify-between md:px-10 lg:px-14">
-			<p className="font-medium">
-				{t("footer.copyright", { year: new Date().getFullYear() })}
-			</p>
-			<div className="flex items-center gap-4">
-				<Link to="/terms" className="hover:underline">
-					{t("footer.terms")}
-				</Link>
-				<Link to="/privacy" className="hover:underline">
-					{t("footer.privacy")}
-				</Link>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="text-foreground/70 flex flex-col items-center gap-4 border-t px-4 py-6 text-xs md:flex-row md:justify-between md:px-10 md:py-8 lg:px-14">
+      <p className="font-medium">
+        {t("footer.copyright", { year: new Date().getFullYear() })}
+      </p>
+      <div className="flex items-center gap-4">
+        <Link to="/terms" className="hover:underline">
+          {t("footer.terms")}
+        </Link>
+        <Link to="/privacy" className="hover:underline">
+          {t("footer.privacy")}
+        </Link>
+      </div>
+    </footer>
+  );
 }
